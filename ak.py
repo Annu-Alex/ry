99c09a6d5a1f9dd3b06597a8ccb42cd9f4d46eed
Annu-Alex <annualex210@gmail.com>
2024-10-18 16:22:45 +0530

    Add files via upload

---
+++ ak.py
@@ -0,0 +1,8 @@
+import math
+number=int(input("Enter a number:"))
+square_root=math.sqrt(number)
+print("square root of ",number,":",square_root)
+y=math.factorial(number)
+print(y)
+x=math.pow(number,2)
+print(x)

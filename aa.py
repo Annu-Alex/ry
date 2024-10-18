79dba22b795e5000204af7167ba62923d52d445c
Annu-Alex <annualex210@gmail.com>
2024-10-18 16:02:47 +0530

    Add files via upload

---
+++ aa.py
@@ -0,0 +1,10 @@
+num1=int(input("enter a number"))
+num2=int(input("enter another number"))
+SUM=num1+num2
+print ("Sum=",SUM)
+DIV=num1/num2
+print ("Div=",DIV)
+print(num1>num2)
+print(num1==num2)
+print("logical AND:",num1>0 and num2>num1)
+print("Logical OR",num1>0 or num2>0)
